7d418a1891b3069281112d1b8be55afd5654525a
Patryk Orłowski <orzeleagle122@gmail.com>
2022-01-06 13:30:10 +0100

    35712

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Viewers = () => {
     return (
-        <div>
-            Viewers
-        </div>
+        <Container>
+            <Wrap>
+                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+            </Wrap>
+            <Wrap>
+                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+            </Wrap>
+            <Wrap>
+                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+            </Wrap>
+            <Wrap>
+                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+            </Wrap>
+            <Wrap>
+                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+            </Wrap>
+        </Container>
     );
 };
 
 export default Viewers;
+
+const Container = styled.div`
+  width: 100%;
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  grid-template-rows: 1fr;
+
+`;
+
+const Wrap = styled.div`
+  img{
+    width: 100%;
+  }
+`;
