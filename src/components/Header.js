149e285581c30bf56bbe16606e6fdd9a3962e5d6
Patryk Orłowski <orzeleagle122@gmail.com>
2022-01-06 13:16:56 +0100

    add img slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,13 +38,14 @@ const Header = () => {
 
 export default Header;
 
-const Nav = styled.div`
+const Nav = styled.nav`
   height: 70px;
   background-color: #090b13;
   display: flex;
   justify-content: space-between;
   align-content: center;
   padding: 0 36px;
+  overflow-x: hidden;
 `;
 
 const Logo = styled.img`
@@ -81,7 +82,7 @@ const NavMenu = styled.div`
         opacity: 0;
         transform: scaleX(0);
         transform-origin: left center;
-        transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
       }
     }
 
