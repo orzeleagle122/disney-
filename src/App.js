97bff91f5d8741bf3cbdc9bd8ff046b7af70a502
Patryk Orłowski <orzeleagle122@gmail.com>
2022-01-07 12:19:59 +0100

    react router dom - 42304

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-      <Home/>
-    </div>
-  );
+    return (
+
+        <div className="App">
+            <Header/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+
+    );
 }
 
 export default App;
