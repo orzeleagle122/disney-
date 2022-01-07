f0c6c0e1cad643ecc73945a6596cdcf639d5cfbe
Patryk Orłowski <orzeleagle122@gmail.com>
2022-01-07 10:47:15 +0100

    add viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -8,16 +8,16 @@ const Viewers = () => {
                 <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
             </Wrap>
             <Wrap>
-                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+                <img src={`/images/viewers-pixar.png`} alt={`viewers`}/>
             </Wrap>
             <Wrap>
-                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+                <img src={`/images/viewers-marvel.png`} alt={`viewers`}/>
             </Wrap>
             <Wrap>
-                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+                <img src={`/images/viewers-starwars.png`} alt={`viewers`}/>
             </Wrap>
             <Wrap>
-                <img src={`/images/viewers-disney.png`} alt={`viewers`}/>
+                <img src={`/images/viewers-national.png`} alt={`viewers`}/>
             </Wrap>
         </Container>
     );
@@ -31,11 +31,26 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(5, minmax(0, 1fr));
   grid-template-rows: 1fr;
-
+  grid-gap: 25px;
+  padding: 30px 0 26px;
 `;
 
 const Wrap = styled.div`
+  border: 3px solid rgba(249,249,249,0.1);
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  transition: all 250ms cubic-bezier(0.25,0.46,0.45,0.94) 0s;
+  
   img{
     width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  
+  &:hover{
+    transform: scale(1.05);
+    border-color: rgba(249,249,249,0.8);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
   }
 `;
